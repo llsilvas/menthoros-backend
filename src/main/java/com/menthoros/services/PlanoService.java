4b2a0b415ba05785d2566ed128e144dd39eb64ff
Leandro Silva <lsilva.info@gmail.com>
2025-10-09 15:08:05 -0300

    feat: Introduce ModoGeracaoPlano enum and update PlanoService

--- src/main/java/com/menthoros/services/PlanoService.java
+++ src/main/java/com/menthoros/services/PlanoService.java
@@ -1,13 +1,15 @@
 package com.menthoros.services;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.menthoros.entity.PlanoSemanal;
+import com.menthoros.enums.ModoGeracaoPlano;
+import jakarta.transaction.Transactional;
 
 import java.util.UUID;
 
 public interface PlanoService {
-    PlanoSemanal gerarPlanoTreino(UUID atletaId) throws JsonProcessingException;
+    @Transactional
+    PlanoSemanal gerarPlanoTreino(UUID atletaId, ModoGeracaoPlano modoGeracao);
 
     void deletePlanoSemanal(UUID planoSemanalId);
 }
