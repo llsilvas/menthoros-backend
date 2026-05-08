e249e53ce613870eaa290d52e710d92040f818a3
Leandro Silva <lsilva.info@gmail.com>
2026-05-08 10:45:16 -0300

    chore: remove old DTO package com.menthoros.api.dtos (migrated to standard package)

--- src/main/java/com/menthoros/api/dtos/DiaAdesaoDto.java
+++
@@ -1,9 +0,0 @@
-package com.menthoros.api.dtos;
-
-public record DiaAdesaoDto(
-    String data,
-    String diaSemana,
-    Integer treinosPlanejados,
-    Integer treinosRealizados,
-    Double percentual
-) {}
