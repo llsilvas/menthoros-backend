e454e774370faeec83f2a157914878978e7e5680
Leandro Silva <lsilva.info@gmail.com>
2026-05-20 16:21:56 -0300

    test(test): otimiza imports no MultiTenantIsolationTest

--- src/test/java/br/com/menthoros/backend/services/impl/WorkoutAnalysisListenerTest.java
+++ src/test/java/br/com/menthoros/backend/services/impl/WorkoutAnalysisListenerTest.java
@@ -23,6 +23,7 @@ import org.springframework.core.io.ResourceLoader;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
